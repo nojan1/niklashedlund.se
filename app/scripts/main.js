c3c23a7eb4753c0438dc3f418d8ee83ae9452c52
Niklas Hedlund <nojan1989@gmail.com>
2015-11-16 19:20:13 +0100

    Added menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,8 @@
  $(document).ready(function(){
-     $('#fullpage').fullpage();
+     $('#fullpage').fullpage({
+        anchors: ['me', 'cv', 'portfolio', 'contact'],
+        menu: '#menu',
+        css3: true,
+        scrollingSpeed: 1000
+     });
  });
